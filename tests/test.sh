1bdcc7481d46b48cebc489122a742f1e30b663a6
Latianna <davidlat024@gmail.com>
2026-07-17 01:07:24 -0400

    updated test.sh file

--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,13 @@
+
 #!/bin/bash
-
 # pytest is baked into the environment image (environment/Dockerfile).
-pytest /tests/test_outputs.py -rA
+mkdir -p /logs/verifier
 
+pytest /tests/test_outputs.py --ctrf=ctrf.json > /logs/verifier/test_output.txt
+
+# Write reward based on test outcome
 if [ $? -eq 0 ]; then
-  echo 1 > /app/reward.txt
+    echo 1 > /logs/verifier/reward.txt
 else
-  echo 0 > /app/reward.txt
+    echo 0 > /logs/verifier/reward.txt
 fi
